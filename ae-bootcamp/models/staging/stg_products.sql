581b982f08be9ec9dc52b58918de96b2503a6a4e
Rahul Prasad <rahul.prasad@datatonic.com>
2021-12-18 18:07:39 +0000

    Removed ; issue from supplier_ids

--- ae-bootcamp/models/staging/stg_products.sql
+++ ae-bootcamp/models/staging/stg_products.sql
@@ -1,6 +1,20 @@
 with source as (
-
-    select * from {{ source('northwind', 'products') }}
+    select cast(supplier_ids as integer) as supplier_id,
+           id,
+           product_code,
+           product_name,
+           description,
+           standard_cost,
+           list_price,
+           reorder_level,
+           target_level,
+           quantity_per_unit,
+           discontinued,
+           minimum_reorder_quantity,
+           category,
+           attachments,
+    from {{ source('northwind', 'products') }}
+    where supplier_ids not like "%;%"
 )
 select 
     *,
